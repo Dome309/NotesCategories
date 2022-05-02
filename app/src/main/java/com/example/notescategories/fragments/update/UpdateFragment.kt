e4003dde770393ec4ecb8fd0f83f38126677f04a
Dome309 <domy1riz@gmail.com>
2022-05-02 15:40:17 +0200

    Added "delete note" function

--- app/src/main/java/com/example/notescategories/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/notescategories/fragments/update/UpdateFragment.kt
@@ -1,12 +1,11 @@
 package com.example.notescategories.fragments.update
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -21,7 +20,7 @@ import kotlinx.android.synthetic.main.fragment_update.view.*
 class UpdateFragment : Fragment() {
 
     private val args by navArgs<UpdateFragmentArgs>()
-    private lateinit var mUserViewModel : UserViewModel
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,19 +35,22 @@ class UpdateFragment : Fragment() {
         view.updateLastName_et.setText(args.currentUser.lastName)
         view.updateAge_et.setText(args.currentUser.age.toString())
 
-        view.update_btn.setOnClickListener{
+        view.update_btn.setOnClickListener {
             updateItem()
         }
+
+        //Add menu
+        setHasOptionsMenu(true)
 
         return view
     }
 
-    private fun updateItem(){
+    private fun updateItem() {
         val firstName = updateFirstName_et.text.toString()
         val lastName = updateLastName_et.text.toString()
         val age = Integer.parseInt(updateAge_et.text.toString())
 
-        if(inputCheck(firstName, lastName, updateAge_et.text)){
+        if (inputCheck(firstName, lastName, updateAge_et.text)) {
             //Create user object
             val updatedUser = User(args.currentUser.id, firstName, lastName, age)
             //Update current user
@@ -56,13 +58,40 @@ class UpdateFragment : Fragment() {
             Toast.makeText(requireContext(), "Updated Successfully!", Toast.LENGTH_SHORT).show()
             //Navigate back
             findNavController().navigate(R.id.action_updateFragment_to_listFragment)
-        }else{
-            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields.", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
         return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete) {
+            deleteUser()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteUser() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            mUserViewModel.deleteUser(args.currentUser)
+            Toast.makeText(requireContext(), "Successfully removed: ${args.currentUser.firstName}", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        builder.setNegativeButton("No") { _, _ ->
+
+        }
+        builder.setTitle("Delete ${args.currentUser.firstName}?")
+        builder.setMessage("Are you sure you want delete ${args.currentUser.firstName}?")
+        builder.create().show()
     }
 
 }
